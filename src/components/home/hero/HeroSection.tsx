3414b71a2e881d0cf8dab6af6335595a03c70d79
jyush98 <55372314+jyush98@users.noreply.github.com>
2025-01-12 17:05:10 -0500

    Decorations - need to return to

--- src/components/home/hero/HeroSection.tsx
+++ src/components/home/hero/HeroSection.tsx
@@ -2,16 +2,25 @@ import React from "react";
 import ProfileImage from "./ProfileImage";
 import HeroContent from "./HeroContent";
 import "../../../styles/hero-section.css"
+// import HeroDecorations from "./HeroDecorations";
+// import HeroSnowDecorations from "./HeroSnowDecorations";
+// import HeroStarDecorations from "./HeroStarDecorations";
 // import AsciiArtAnimation from "./AsciiArtAnimation";
 
 const HeroSection: React.FC = () => {
-  return (
-    <div className="hero-section" id="hero-container">
-      <ProfileImage />
-      {/* <AsciiArtAnimation/> */}
-      <HeroContent />
-    </div>
-  );
+    return (
+        <div className="hero-section" id="hero-container">
+            {/* <div style={{ gridColumn: "1 / span 3" }}>
+                <HeroStarDecorations />
+            </div> */}
+            <ProfileImage />
+            {/* <AsciiArtAnimation/> */}
+            <HeroContent />
+            {/* <div style={{ gridColumn: "10 / span 3" }}>
+                <HeroStarDecorations />
+            </div> */}
+        </div>
+    );
 };
 
 export default HeroSection;
---
+++ src/components/home/hero/HeroSnowDecorations.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../../styles/hero-snow-decorations.css";
+
+const HeroSnowDecorations: React.FC = () => {
+    const snowflakes = Array.from({ length: 15 }); // Create 15 snowflakes
+  
+    return (
+      <div className="hero-snow-decorations">
+        {snowflakes.map((_, index) => {
+          const isLeft = Math.random() > 0.5; // Randomly decide left or right
+          const position = isLeft
+            ? Math.random() * 25 // For left columns
+            : 75 + Math.random() * 25; // For right columns
+  
+          return (
+            <div
+              key={index}
+              className={`snowflake ${isLeft ? "snowflake-left" : "snowflake-right"}`}
+              style={{
+                left: `${position}vw`, // Dynamically set left position
+                animationDelay: `${Math.random() * 3}s`, // Staggered starts
+                fontSize: `${Math.random() * 20 + 10}px`, // Random size between 10px and 30px
+              }}
+            >
+              *
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  export default HeroSnowDecorations;
