4ba4d3e3eeff9d58c9b0b4ff59f5a598a933f9bc
jyush98 <55372314+jyush98@users.noreply.github.com>
2025-01-17 11:29:27 -0500

    resume experience updates

--- src/components/home/experience/ResumeExperienceCard.tsx
+++ src/components/home/experience/ResumeExperienceCard.tsx
@@ -2,27 +2,31 @@ import React from "react";
 import "./ResumeExperienceCard.css";
 
 interface ResumeExperienceCardProps {
-  date: string;
-  title: string;
-  company: string;
-  description: string;
+    date: string;
+    title: string;
+    company: string;
+    description: [];
 }
 
 const ResumeExperienceCard: React.FC<ResumeExperienceCardProps> = ({ date, title, company, description }) => {
-  return (
-    <div className="resume-card">
-      {/* Left Column: Date */}
-      <div className="resume-card-date">
-        <p>{date}</p>
-      </div>
-      {/* Right Column: Title, Company, Experience */}
-      <div className="resume-card-content">
-        <h3 className="resume-card-title">{title}</h3>
-        <h4 className="resume-card-company">{company}</h4>
-        <p className="resume-card-description">{description}</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="resume-card">
+            {/* Left Column: Date */}
+            <div className="resume-card-date">
+                <p>{date}</p>
+            </div>
+            {/* Right Column: Title, Company, Experience */}
+            <div className="resume-card-content">
+                <h3 className="resume-card-title">{title}</h3>
+                <h4 className="resume-card-company">{company}</h4>
+                <ul className="bulleted-description">
+                    {description.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default ResumeExperienceCard;
--- src/content/resumeContent.ts
+++ src/content/resumeContent.ts
@@ -1,20 +1,36 @@
 export const resumeExperiences = [
-    {
-      company: "Jason & Co.",
-      title: "Co-Founder",
-      date: "May 2024 - Current",
-      description: "Developed scalable web applications using React and Node.js. Developed scalable web applications using React and Node.js. Developed scalable web applications using React and Node.js. Developed scalable web applications using React and Node.js. Developed scalable web applications using React and Node.js. Developed scalable web applications using React and Node.js.",
-    },
-    {
-      company: "QVT Financial LP",
-      title: "FullStack Software Engineer",
-      date: "July 2022 - April 2023",
-      description: "Built APIs and optimized server-side processes for performance. Built APIs and optimized server-side processes for performance. Built APIs and optimized server-side processes for performance. Built APIs and optimized server-side processes for performance. Built APIs and optimized server-side processes for performance. Built APIs and optimized server-side processes for performance. Built APIs and optimized server-side processes for performance.",
-    },
-    {
-      company: "Better Mortgage",
-      title: "Backend Developer",
-      date: "June 2022 - June 2023",
-      description: "Collaborated on user-facing features and fixed critical bugs. Collaborated on user-facing features and fixed critical bugs. Collaborated on user-facing features and fixed critical bugs. Collaborated on user-facing features and fixed critical bugs. Collaborated on user-facing features and fixed critical bugs. Collaborated on user-facing features and fixed critical bugs. Collaborated on user-facing features and fixed critical bugs.",
-    },
-  ];
+  {
+    company: "Jason & Co.",
+    title: "Co-Founder",
+    date: "May 2023 - Present",
+    description: [
+      "Designed and launched an e-commerce website that boosted online sales by 35%",
+      "Improved processes supply chain and inventory management processes, cutting lead times by 20% and enhancing product availability",
+      "Executed targeted Instagram campaigns leveraging audience insights and engagement analytics to grow followers by 15% and boost interaction rates by 25% over six months, elevating brand awareness and engagement",
+
+    ],
+  },
+  {
+    company: "QVT Financial LP",
+    title: "FullStack Software Engineer",
+    date: "July 2022 - April 2023",
+    description: [
+      "Collaborated with stakeholders to refine product requirements, crafting an Agile roadmap that prioritized essential features for the React application, leading to successful launch within a tight 3-month development timeline",
+      "Automated the reconciliation process between the performance and the accounting data records by building dashboards to detect discrepancies, saving the accounting team 20+ hours a month; improved on the dashboard as end-users requested features",
+      "Diagnosed and resolved 10 critical application bugs in partnership with accounts, leading to a 15% increase in application reliability",
+      "Spearheaded the planning and integration of new application features based on user feedback, reducing time spent on these applications by 20%",
+      "Developed a toolkit 20 Excel functions written in C# yielding a 40% reduction in analysis time and enhancing overall data accuracy for financial reports",
+    ],
+  },
+  {
+    company: "Better Mortgage",
+    title: "Backend Developer",
+    date: "June 2022 - June 2023",
+    description: [
+      "Built and tested pricing engine with the capacity to handle 2,000 requests per second (up from 15)",
+      "Developed in-house parity testing tool to evaluate accuracy of the new pricing engine, enabling the launch of 100+ new products",
+      "Led and Organized bi-weekly demo sessions for 25+ team members, sharing insights on new tools and advancements, fostering a culture of collaboration and continuous improvement",
+      "Served as Marketplace Goalie; identifying and rectifying bugs impacting customer loan accessibility",
+    ],
+  },
+];
