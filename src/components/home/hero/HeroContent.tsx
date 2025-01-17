b70f8760adf014912c9d1f076e0c1f2d836fa4e5
jyush98 <55372314+jyush98@users.noreply.github.com>
2025-01-17 12:48:48 -0500

    hero content added

--- src/components/home/hero/HeroContent.tsx
+++ src/components/home/hero/HeroContent.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { heroContent } from "../../../content/heroContent";
 
 const HeroContent: React.FC = () => {
   return (
     <section className="hero-content">
       <h1 className="hero-heading figtree-heading">Jonathan Yushuvayev</h1>
       <h2 className="hero-subheading ibm-plex-mono-medium-italic text-blue">FullStack Software Engineer</h2>
-      <p className="hero-about figtree-body"> A short description about yourself. This can include your professional
-      background, interests, and what you're passionate about. A short description about yourself. This can include your professional
-      background, interests, and what you're passionate about.</p>
+      <p className="hero-about figtree-body">
+        {heroContent.blurb.split("\n").map((line, index) => (
+          <span key={index}>{line}<br /></span>
+        ))}
+      </p>
     </section>
   );
 };
